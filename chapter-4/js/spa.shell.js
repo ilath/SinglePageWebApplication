97471203f48b9c06baa7d0f47b93566e37253bd1
ilath <leisurely1001@outlook.com>
2018-06-05 22:08:36 +0800

    Signed-off-by: ilath

--- chapter-4/js/spa.shell.js
+++ chapter-4/js/spa.shell.js
@@ -7,19 +7,19 @@ spa.shell = (function() {
           closed: true
         }
       },
-      main_html: String() +
-        '<div class="spa-shell-head">' +
-        '<div class="spa-shell-head-logo"></div>' +
-        '<div class="spa-shell-head-acct"></div>' +
-        '<div class="spa-shell-head-search"></div>' +
-        '</div>' +
-        '<div class="spa-shell-main">' +
-        '<div class="spa-shell-main-nav"></div>' +
-        '<div class="spa-shell-main-content"></div>' +
-        '</div>' +
-        '<div class="spa-shell-foot"></div>' +
-        '<div class="spa-shell-chat"></div>' +
-        '<div class="spa-shell-modal"></div>',
+      main_html: String()
+        + '<div class="spa-shell-head">'
+          + '<div class="spa-shell-head-logo"></div>'
+          + '<div class="spa-shell-head-acct"></div>'
+          + '<div class="spa-shell-head-search"></div>'
+        + '</div>'
+        + '<div class="spa-shell-main">'
+          + '<div class="spa-shell-main-nav"></div>'
+          + '<div class="spa-shell-main-content"></div>'
+        + '</div>'
+        + '<div class="spa-shell-foot"></div>'
+        + '<div class="spa-shell-chat"></div>'
+        + '<div class="spa-shell-modal"></div>',
       chat_extend_time: 1000,
       chat_retract_time: 300,
       chat_extend_height: 450,
