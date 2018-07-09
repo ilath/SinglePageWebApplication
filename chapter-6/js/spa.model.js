fcc7de21e43337f813c47607f96cd55daa97b1ea
ilath <leisurely1001@outlook.com>
2018-07-09 22:30:18 +0800

    Signed-off-by: ilath

--- chapter-6/js/spa.model.js
+++ chapter-6/js/spa.model.js
@@ -44,7 +44,7 @@ spa.model = ( function () {
     stateMap.user.id  = user_map._id;
     stateMap.css_map  = user_map.css_map;
     stateMap.people_cid_map[ user_map._id ] = stateMap.user;
-
+    chat.join();
     $.gevent.publish( 'spa-login', [ stateMap.user ] );
   };
 
@@ -118,6 +118,7 @@ spa.model = ( function () {
     logout = function () {
       var is_removed, user = stateMap.user;
 
+      chat._leave();
       is_removed    = removePerson( user );
       stateMap.user = stateMap.anon_user;
 
@@ -136,12 +137,17 @@ spa.model = ( function () {
 
   chat = (function() {
     var
-      _publish_listchange,
-      _update_list, _leave_chat, join_chat;
+      _publish_listchange, _publish_updatechat,
+      _update_list, _leave_chat,
+
+      get_chatee, join_chat, send_msg, set_chatee,
+
+      chatee = null;
 
     _update_list = function( arg_list ) {
       var i, person_map, make_person_map,
-          people_list = arg_list[0];
+          people_list      = arg_list[0],
+          is_chatee_online = false;
 
       clearPeopleDb();
 
@@ -164,9 +170,15 @@ spa.model = ( function () {
         name    : person_map.name
       };
 
+      if( chatee && chatee.id === make_person_map.id ){
+        is_chatee_online = true;
+      }
+
       makePerson( make_person_map );
 
       stateMap.people_db.sort( 'name' );
+
+      if( chatee && ! is_chatee_online ) { set_chatee(''); }
     };
 
     _publish_listchange = function( arg_list ) {
@@ -174,11 +186,24 @@ spa.model = ( function () {
       $.gevent.publish( 'spa-listchange', [ arg_list ] );
     };
 
+    _publish_updatechat = function( arg_list ) {
+      var msg_map = arg_list[ 0 ];
+
+      if( ! chatee ) { set_chatee( msg_map.sender_id ); }
+      else if ( msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id)
+      { set_chatee( msg_map.sender_id ); }
+
+      $.gevent.publish( 'spa-updatechat', [ msg_map ] );
+    };
+
     _leave_chat = function () {
       var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      chatee  = null;
       stateMap.is_connected = false;
       if ( sio ) { sio.emit( 'leavechat' ); }
     };
+
+    get_chatee = function () { return chatee; };
 
     join_chat = function () {
       var sio;
@@ -192,19 +217,60 @@ spa.model = ( function () {
 
       sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
       sio.on( 'listchange', _publish_listchange );
+      sio.on( 'updatechat', _publish_updatechat );
       stateMap.is_connected = true;
       return true;
     };
 
+    send_msg = function ( msg_text ) {
+      var msg_map,
+        sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+      if( ! sio ) { return false; }
+      if( ! ( stateMap.user && chatee ) ) { return false; }
+
+      msg_map = {
+        dest_id   : chatee.id,
+        dest_name : chatee.name,
+        sender_id : stateMap.user_id,
+        msg_text  : msg_text
+      };
+
+      _public_updatechat( [ msg_map ] );
+      sio.emit( 'updatechat', msg_map );
+      return true;
+    };
+
+    set_chatee = function ( person_id ) {
+      var new_chatee;
+      new_chatee = stateMap.people_cid_map[ person_id ];
+      if ( new_chatee ){
+        if ( chatee && chatee.id === new_chatee.id ){
+          return false;
+        }
+      }
+      else{
+        new_chatee = null;
+      }
+
+      $.gevent.publish( 'spa-set_chatee',
+        { old_chatee : chatee, new_chatee : new_chatee }
+      );
+      chatee = new_chatee;
+      return true;
+    };
+
     return {
-      _leave  : _leave_chat,
-      join    : join_chat
+      _leave     : _leave_chat,
+      get_chatee : get_chatee,
+      join       : join_chat,
+      send_msg   : send_msg,
+      set_chatee : set_chatee
     };
 
   } () );
 
   initModule = function () {
-    var i, people_list, person_map;
 
     stateMap.anon_user = makePerson({
       cid   : configMap.anon_id,
@@ -212,19 +278,6 @@ spa.model = ( function () {
       name  : 'anonymous'
     });
     stateMap.user = stateMap.anon_user;
-
-    if( isFakeData ) {
-      people_list = spa.fake.getPeopleList();
-      for ( i = 0; i < people_list.length; i++ ) {
-        person_map = people_list[ i ];
-        makePerson({
-          cid     : person_map._id,
-          css_map : person_map.css_map,
-          id      : person_map._id,
-          name    : person_map.name
-        });
-      }
-    }
   };
 
   return {
