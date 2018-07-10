c3a893e92e6a681ad32d6a9f3910165aa5f373be
ilath <leisurely1001@outlook.com>
2018-07-10 23:43:29 +0800

    Signed-off-by: ilath

--- chapter-6/js/spa.model.js
+++ chapter-6/js/spa.model.js
@@ -260,12 +260,20 @@ spa.model = ( function () {
       return true;
     };
 
+    update_avatar = function ( avatar_update_map ){
+      var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      if ( sio ) {
+        sio.emit( 'updateavatar', avatar_update_map );
+      }
+    };
+
     return {
-      _leave     : _leave_chat,
-      get_chatee : get_chatee,
-      join       : join_chat,
-      send_msg   : send_msg,
-      set_chatee : set_chatee
+      _leave        : _leave_chat,
+      get_chatee    : get_chatee,
+      join          : join_chat,
+      send_msg      : send_msg,
+      set_chatee    : set_chatee,
+      update_avatar : update_avatar
     };
 
   } () );
